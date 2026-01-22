f26f5d09e670166616fd1b7ff972148cf72c1fcb
Drew Early <jae0025@uah.edu>
2026-01-22 15:47:31 -0600

    Initial React-Leaflet setup. It's ready for mapping!!!

--- containers/pirate-frontend/src/components/PirateMap.js
+++ containers/pirate-frontend/src/components/PirateMap.js
@@ -1,11 +1,19 @@
-// Imports here
+import 'leaflet/dist/leaflet.css';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 function PirateMap() {
   return (
-    <>
-      <h1>This is where we start building the app!</h1>
-      <p>Put your HTML here!</p>
-    </>
+    <MapContainer style={{position: "absolute", width: "100%", height: "100%"}} center={[34.7190616534629, -86.64664978111168]} zoom={13}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[34.7190616534629, -86.64664978111168]}>
+        <Popup>
+          CS499 - Senior Design
+        </Popup>
+      </Marker>
+    </MapContainer>
   )
 }
 
