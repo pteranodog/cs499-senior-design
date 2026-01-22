ea65dc31a04339ad5aaa468e17db2433960bcf71
Drew Early <jae0025@uah.edu>
2026-01-22 14:51:28 -0600

    Strip out all of the React defaults

---
+++ containers/pirate-frontend/src/components/PirateMap.js
@@ -0,0 +1,12 @@
+// Imports here
+
+function PirateMap() {
+  return (
+    <>
+      <h1>This is where we start building the app!</h1>
+      <p>Put your HTML here!</p>
+    </>
+  )
+}
+
+export default PirateMap;
