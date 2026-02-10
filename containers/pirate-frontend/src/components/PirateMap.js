5a7d61ace162dc833ab68a86835ac42667682dc2
Drew Early <jae0025@uah.edu>
2026-02-10 10:48:47 -0600

    Controls overlay (Top right only, but can be copied)

--- containers/pirate-frontend/src/components/PirateMap.js
+++ containers/pirate-frontend/src/components/PirateMap.js
@@ -1,18 +1,25 @@
 import 'leaflet/dist/leaflet.css';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import TopRightControls from './controls/TopRightControls.js';
 
 function PirateMap() {
   return (
     <MapContainer style={{position: "absolute", width: "100%", height: "100%"}} center={[34.7190616534629, -86.64664978111168]} zoom={13}>
+      {/* Base layer source. Maybe update later to include additional options? */}
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+
+      {/* A marker at the location of the Senior Design Class (EXAMPLE ONLY - REMOVE LATER) */}
       <Marker position={[34.7190616534629, -86.64664978111168]}>
         <Popup>
           CS499 - Senior Design
         </Popup>
       </Marker>
+
+      {/* Controls that float above the map in the top right */}
+      <TopRightControls/>
     </MapContainer>
   )
 }
