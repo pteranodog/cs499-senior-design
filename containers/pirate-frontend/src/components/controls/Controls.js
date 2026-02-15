5c34efa42fc0c64d297b3e534171cdb330f2a2bf
eleague3025 <league.elijah@yahoo.com>
2026-02-15 11:18:52 -0600

    Legend added for distinguishability between participants. Can be deleted later if not needed

--- containers/pirate-frontend/src/components/controls/Controls.js
+++ containers/pirate-frontend/src/components/controls/Controls.js
@@ -517,6 +517,55 @@ return (
       </div>
     </Control>
 
+    {/* LEGEND */}
+    <Control prepend position = "bottomleft">
+      <div
+        className="bg-light text-dark p-3 rounded shadow"
+        style={{ minWidth: "160px"}}
+      >
+        <h5 className = "mb-2">Legend</h5>
+
+        {/* MERCHANTS */}
+        <div className="d-flex align-items-center mb-1">
+          <div
+            style={{
+              width: "20px",
+              height: "20px",
+              backgroundColor: "green",
+              marginRight: "8px",
+            }}
+          ></div>
+            <span>Maerchants</span>
+          </div>
+
+          {/* PIRATES */}
+          <div className="d-flex align-items-center mb-1">
+            <div
+              style={{
+                width: "20px",
+                height: "20px",
+                backgroundColor: "red",
+                marginRight: "8px",
+              }}
+            ></div>
+            <span>Pirates</span>
+          </div>
+
+          {/* Security Ships */}
+          <div className="d-flex align-items-center mb-1">
+            <div
+              style={{
+                width: "20px",
+                height: "20px",
+                backgroundColor: "blue",
+                marginRight: "8px",
+              }}
+            ></div>
+            <span>Security</span>
+          </div>
+        </div>
+    </Control>  
+
     {/* SIMULATION END SCREEN */}
     {showEndScreen && (
       <div
