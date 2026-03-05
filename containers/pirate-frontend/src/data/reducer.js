1d9812823767240dcff8aa89ed8eb0114a85d160
Drew Early <jae0025@uah.edu>
2026-03-05 13:32:39 -0600

    Functionalized data structures

---
+++ containers/pirate-frontend/src/data/reducer.js
@@ -0,0 +1,50 @@
+// This file is NOT YET the reducer function.
+// Temporarily using it to store code that isn't needed in classes.js
+
+function releaseSkiff(id) {
+  if (this.heldSkiffs) {    
+    releasedSkiff = new PirateShip(id, this.pos, "small")
+    this.crewSize -= 7
+    this.heldSkiffs -= 1
+    return this.releaseSkiff // Return this new ship so it can be added to the state data
+  }
+  else {
+    console.log("Pirate ship w/ id " + this.id + " has no skiffs to release.")
+  }
+}
+
+// TODO: Remove this once it's in the reducer function
+// Add method to release small patrol ships (for large patrol ships)
+function releaseSmallPatrolShip(id) {
+  if (this.carriedSmallPatrols) {   
+    releasedPatrol = new PatrolShip(id, this.pos, "small")
+    this.crewSize -= 4
+    this.carriedSmallPatrols -= 1
+    return releasedPatrol // Return this new ship so it can be added to the state data
+  }
+  else {
+    console.log("Patrol ship w/ id " + this.id + " has no small patrol ships to release.")
+  }
+}
+
+// TODO: Reducer function
+// Spawn a new merchant ship at this port's location.
+function spawnMerchant(id, size) {
+  thisMerchant = new MerchantShip(id, this.pos, size, this)
+  return thisMerchant
+}
+
+// TODO: Reducer function
+// Spawn a new patrol ship at this port's location.
+function spawnPatrol(id, size) {
+  thisPatrol = new PatrolShip(id, this.pos, size, this)
+  return thisPatrol
+}
+
+
+// TODO: Reducer function
+// Spawn a new pirate ship at this cove's location.
+function spawnPirate(id, size) {
+  thisPirate = new PirateShip(id, this.pos, size, this)
+  return thisPirate
+}
