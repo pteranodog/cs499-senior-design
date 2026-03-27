e208da31a9d13ec09a70af5c8b7aea0be4279e41
Aidan5775 <alpotter0704@gmail.com>
2026-03-27 16:30:14 -0500

    Created the time viewer display. Presently displays gibberish. Created the LiveCounts display(presently does not pass in anything.) Updated RunControls so it displays both.

---
+++ containers/pirate-frontend/src/components/controls/active-run/LiveCounts.js
@@ -0,0 +1,29 @@
+import Dropdown from 'react-bootstrap/Dropdown';
+
+export default function LiveCounts({ simState }) {
+    return (
+        <Dropdown
+        bg="light"
+        text="dark"
+        className="p-2 small"
+        style={{
+            position: 'absolute',
+            top: 60,
+            right: 16,
+            zIndex: 1000,
+        }}
+        >
+                  <Dropdown.Toggle variant="light" size="sm">
+                    View Live Counts
+                  </Dropdown.Toggle>
+                  <Dropdown.Menu>
+                    <Dropdown.Item disabled>Entries: {simState.entries}</Dropdown.Item>
+                    <Dropdown.Item disabled>Exits: {simState.exits}</Dropdown.Item>
+                    <Dropdown.Item disabled>Captures: {simState.captures}</Dropdown.Item>
+                    <Dropdown.Item disabled>Defeats: {simState.defeats}</Dropdown.Item>
+                    <Dropdown.Item disabled>Rescues: {simState.rescues}</Dropdown.Item>
+                    <Dropdown.Item disabled>Evasions: {simState.evasions}</Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+    );
+}
