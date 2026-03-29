acad5f0ddd90b1430f9cec170fbd30351a4b52c3
Aidan5775 <alpotter0704@gmail.com>
2026-03-29 16:28:16 -0500

    Fixed the Terminate Button. Now shifts to proper end state.

--- containers/pirate-frontend/src/components/controls/active-run/BottomRightButtons.js
+++ containers/pirate-frontend/src/components/controls/active-run/BottomRightButtons.js
@@ -23,9 +23,8 @@ export default function BottomRightButtons({ simState, modifySimState, runID }) 
     }
 
     modifyRun('status', 'terminated-before-natural-completion');
-    // TODO: SET THIS TO VIEW-END-RUN AGAIN WHEN END-RUN IS DONE
-    modifySimState({ type: 'view-run-list', run: runID });
-    // modifySimState({ type: 'view-run-end', run: runID });
+
+    modifySimState({ type: 'view-run-end', run: runID });
 
   };
 
