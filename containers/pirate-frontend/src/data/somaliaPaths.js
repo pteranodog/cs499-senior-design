c58872ee73f625e4971d94f2f7a65da31517ea2a
jonathanpoling <jep0040@uah.edu>
2026-03-30 05:59:21 -0500

    forgot some function calls

--- containers/pirate-frontend/src/data/somaliaPaths.js
+++ containers/pirate-frontend/src/data/somaliaPaths.js
@@ -16,26 +16,29 @@ let mombasa = toCart(-4.0717, 39.6730);
 let dar_es_salaam = toCart(-6.7640, 39.2747)
 let oman = toCart(16.9, 54.0);
 
-let somaliaPaths = {};
-
-
-let djibouti_To_Mumbai_Path = behaviors.newPath(
+let djibouti_To_Mumbai_Path = behaviors.assemblePath(behaviors.newPath(
     [djibouti, toCart(11.5, 57.3), toCart(13.2, 64.5), mumbai], 
     1  
-);
+));
 
-let djibouti_To_Mombasa_Path = behaviors.newPath(
+let djibouti_To_Mombasa_Path = behaviors.assemblePath(behaviors.newPath(
     [djibouti, toCart(12.6, 49.0), toCart(14.6, 55.6,), toCart(10.7, 59.5), toCart(3.2, 55.2), toCart(-2.1, 47.2), mombasa],
     2
-)
+));
 
-let dar_Es_Salaam_To_Mumbai_Path = behaviors.newPath(
+let dar_Es_Salaam_To_Mumbai_Path = behaviors.assemblePath(behaviors.newPath(
     [dar_es_salaam, toCart(-1.0, 51.3), toCart(5.4, 54.4), toCart(12.0, 54.9), toCart(16.8, 63.4), mumbai], 
     3
-);
+));
 
-let mombasa_To_Oman_Path = behaviors.newPath(
+let mombasa_To_Oman_Path = behaviors.assemblePath(behaviors.newPath(
     [mombasa, toCart(-4.2, 48.4), toCart(0.5, 52.2), toCart(4.6, 56.3), toCart(10.8, 58.6), oman], 
     4
-);
+));
 
+export const somaliaPaths = {
+    djibouti_To_Mombasa_Path: djibouti_To_Mombasa_Path,
+    djibouti_To_Mumbai_Path: djibouti_To_Mumbai_Path,
+    dar_Es_Salaam_To_Mumbai_Path: dar_Es_Salaam_To_Mumbai_Path,
+    mombasa_To_Oman_Path: mombasa_To_Oman_Path
+};
