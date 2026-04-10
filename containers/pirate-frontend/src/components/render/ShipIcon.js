72693d65827dec9c4b63b74c310b96a1a97b861e
Carter Ward <csw0033@uah.edu>
2026-04-09 19:22:49 -0500

    fUCKign Workk

--- containers/pirate-frontend/src/components/render/ShipIcon.js
+++ containers/pirate-frontend/src/components/render/ShipIcon.js
@@ -1,36 +1,7 @@
 import { Marker, Popup } from 'react-leaflet';
-import { useMemo } from 'react';
 import L from 'leaflet';
 
-const BASE_ZOOM = 6;
-const BASE_SCALE = 1.8;
-const MIN_SCALE = 1.2;
-const MAX_SCALE = 3.0;
-
-const ICON_CONFIG = {
-  pirate: {
-    iconUrl: '/pirate-icon.png',
-    iconSize: [156, 120],
-    filter: 'invert(100%) sepia(100%) saturate(10000%) hue-rotate(0deg) brightness(100%) contrast(100%)',
-  },
-  merchant: {
-    iconUrl: '/merchant-icon.png',
-    iconSize: [243, 75],
-    filter: 'invert(100%) sepia(100%) saturate(10000%) hue-rotate(85deg) brightness(100%) contrast(100%)',
-  },
-  patrol: {
-    iconUrl: '/patrol-icon.png',
-    iconSize: [210, 120],
-    filter: 'invert(100%) sepia(100%) saturate(10000%) hue-rotate(190deg) brightness(100%) contrast(100%)',
-  },
-  default: {
-    iconUrl: '/boat-icon.png',
-    iconSize: [180, 180],
-    filter: '',
-  }
-};
-
-function ShipIcons({ type, lat, lon, zoom }) {
+function ShipIcons({ type, lat, lon }) {
   // For now we'll just set marker text using type, later we'll switch it to show different icons
   var markerText = "Unknown vessel type!";
   if (type === "pirate") {
@@ -38,35 +9,42 @@ function ShipIcons({ type, lat, lon, zoom }) {
   } else if (type === "merchant") {
     markerText = "This is a merchant!";
   } else if (type === "patrol") {
-    markerText = "This is a security vessel!";
+    markerText = "This is a patrol vessel!";
   }
 
-  const shipIcon = useMemo(() => {
-    const iconConfig = ICON_CONFIG[type] || ICON_CONFIG.default;
-    const scale = Math.min(
-      MAX_SCALE,
-      Math.max(MIN_SCALE, BASE_SCALE * Math.pow(1.15, zoom - BASE_ZOOM))
-    );
-    const [baseWidth, baseHeight] = iconConfig.iconSize;
-    const width = Math.round(baseWidth * scale);
-    const height = Math.round(baseHeight * scale);
+  const iconMap = {
+    pirate: L.icon({
+      iconUrl: '/pirate-icon.png',
+      iconSize: [52, 40],
+      iconAnchor: [26, 40],
+      popupAnchor: [7, -35]
+    }),
+    merchant: L.icon({
+      iconUrl: '/merchant-icon.png',
+      iconSize: [81, 25],
+      iconAnchor: [41, 25],
+      popupAnchor: [-7, -15]
+    }),
+    patrol: L.icon({
+      iconUrl: '/patrol-icon.png',
+      iconSize: [70, 40],
+      iconAnchor: [35, 40],
+      popupAnchor: [-5, -35]
+    }),
+    default: L.icon({
+      iconUrl: '/boat-icon.png',
+      iconSize: [60, 60],
+      iconAnchor: [26, 40],
+      popupAnchor: [-14, -25]
+    })
+  };
 
-    const filterStyle = iconConfig.filter ? `filter:${iconConfig.filter};` : '';
-
-    return L.divIcon({
-      className: '',
-      html: `<img src="${iconConfig.iconUrl}" width="${width}" height="${height}" style="${filterStyle}" />`,
-      iconSize: [width, height],
-      iconAnchor: [Math.round(width / 2), height],
-      popupAnchor: [0, -height + 5],
-    });
-  }, [type, zoom]);
-
+  const shipIcon = iconMap[type]|| iconMap.default;
   return (
-    <Marker position={[lat, lon]} icon={shipIcon}>                                                         
-      <Popup>                                                                                                          
+    <Marker position={[lat, lon]} icon={shipIcon}>
+      <Popup>
         {markerText}
-      </Popup>                                                                                                         
+      </Popup>
     </Marker>
   )
 }
