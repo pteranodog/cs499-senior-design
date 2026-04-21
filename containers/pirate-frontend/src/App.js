874bb4ae886a5be994b038b6e83bc6d44355011e
Carter Ward <csw0033@uah.edu>
2026-04-21 17:00:50 -0500

    New fix

--- containers/pirate-frontend/src/App.js
+++ containers/pirate-frontend/src/App.js
@@ -27,10 +27,32 @@ function App() {
     viewportSize.effectiveWidth < MIN_SUPPORTED_WIDTH ||
     viewportSize.effectiveHeight < MIN_SUPPORTED_HEIGHT;
 
+
   if (isMobile) {
     return (
       <div style={{ textAlign: 'center', marginTop: '50px' }}>
         <div className="marquee">
+          <div className="marquee-content">
+            <img src="/patrol-icon.png" alt="Patrol" style={{ height: '50px', marginRight: '40px' }} />
+            <img src="/pirate-icon.png" alt="Pirate" style={{ height: '50px', marginRight: '40px' }} />
+            <img src="/merchant-icon.png" alt="Merchant" style={{ height: '50px' }} />
+          </div>
+        </div>
+        <h1>Please use a desktop browser</h1>
+        <p>This application is not supported on mobile devices.</p>
+      </div>
+    )
+  }
+
+  return (
+    <>
+      <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css"
+        integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7"
+        crossOrigin="anonymous"
+      />
+      <PirateMap />
       {isTooSmall && (
         <div
           style={{
@@ -67,51 +89,6 @@ function App() {
           </p>
         </div>
       )}
-          <div className="marquee-content">
-            <img src="/patrol-icon.png" alt="Patrol" style={{ height: '50px', marginRight: '40px' }} />
-            <img src="/pirate-icon.png" alt="Pirate" style={{ height: '50px', marginRight: '40px' }} />
-            <img src="/merchant-icon.png" alt="Merchant" style={{ height: '50px' }} />
-          </div>
-        </div>
-        <h1>Please use a desktop browser</h1>
-        <p>This application is not supported on mobile devices.</p>
-      </div>
-    )
-  }
-
-  if (isTooSmall) {
-    return (
-      <div style={{ textAlign: 'center', marginTop: '50px' }}>
-        <div className="marquee">
-          <div className="marquee-content">
-            <img src="/patrol-icon.png" alt="Patrol" style={{ height: '50px', marginRight: '40px' }} />
-            <img src="/pirate-icon.png" alt="Pirate" style={{ height: '50px', marginRight: '40px' }} />
-            <img src="/merchant-icon.png" alt="Merchant" style={{ height: '50px' }} />
-          </div>
-        </div>
-        <h1>Your Window is too small!</h1>
-        <p>
-          Please resize your browser window to at least {MIN_SUPPORTED_WIDTH}x{MIN_SUPPORTED_HEIGHT}.
-        </p>
-        <p>
-          Current viewport: {viewportSize.width}x{viewportSize.height} (CSS px)
-        </p>
-        <p>
-          Effective size: {viewportSize.effectiveWidth}x{viewportSize.effectiveHeight} (screen px)
-        </p>
-      </div>
-    );
-  }
-
-  return (
-    <>
-      <link
-        rel="stylesheet"
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css"
-        integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7"
-        crossorigin="anonymous"
-      />
-      <PirateMap />
     </>
   );
 }
